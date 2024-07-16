d542360d97611e400c9d3cfb3ad3aed1d135e7b6
Marta López Caballer <martalop@car12s5.42barcelona.com>
2024-07-16 19:59:59 +0200

    added readline library and did prompt

--- main.c
+++ main.c
@@ -6,13 +6,28 @@
 /*   By: martalop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:34:04 by martalop          #+#    #+#             */
-/*   Updated: 2024/07/16 16:34:40 by martalop         ###   ########.fr       */
+/*   Updated: 2024/07/16 19:39:21 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include "readline/readline.h"
+#include "readline/history.h"
 
 int	main()
 {
-	printf("starting minishell..\n");
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (i < 5)
+	{	
+		str = readline("our minishell > ");
+		printf("user input: %s\n", str);
+		//clasificar str
+		free(str);
+		i++;
+	}
+	rl_clear_history();
+	return (0);
 }
