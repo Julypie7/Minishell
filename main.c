c68fa915ff2967c051c45802f1cddeced2c5cd93
Marta López Caballer <martalop@car10s1.42barcelona.com>
2024-07-19 12:05:12 +0200

    started doing env copy in main, should not compile

--- main.c
+++ main.c
@@ -6,27 +6,58 @@
 /*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:56:35 by ineimatu          #+#    #+#             */
-/*   Updated: 2024/07/18 23:35:25 by martalop         ###   ########.fr       */
+/*   Updated: 2024/07/19 11:31:48 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft/libft.h"
 
-/*void	env_to_list(t_info *info, char **env)
+int	find_equal(char *str)
 {
-	char	*key;
-	char	*value;
+	int	i;
 
-	// splt de '=' de env[i] hasta que ahi haya un NULL?
-}*/
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i++;
+	if (!str[i])
+		return (0);
+	return (i);
+}
+
+void	env_to_list(t_info *info, char **env)
+{
+	char	**arr;
+	int		found_at;
+	int			i;
+	int			j;
+
+	i = 0;
+	while (env[i])
+	{
+		j = 0;
+		// variable name
+		found_at = find_equal(env[i]);
+		arr[j] = ft_substr(env[i], 0, found_at + 1);
+		printf("%s\n", arr[j]);
+		info->
+		j++;
+		// content of variable
+		arr[j] = ft_substr(env[i], found_at + 1, ft_strlen(env[i]) - found_at);
+		printf("%s\n", arr[j]);
+		printf("\n");
+		i++;
+	}
+	arr[j] = NULL;
+
+}
 
 void	init_struct(t_info *info, char **env)
 {
 	(void) env;
 	info->rl = NULL;
 	info->ex_stat = ft_strlen("hola");
-//	env_to_list(info, env);
+	env_to_list(info, env);
 }
 
 void	start_reading(t_info *info)
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 13:00:48 by ineimatu          #+#    #+#             */
-/*   Updated: 2024/07/18 21:15:07 by martalop         ###   ########.fr       */
+/*   Updated: 2024/07/19 12:04:12 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ typedef	struct s_info
 
 // initial functions
 void	start_reading(t_info *info);
+int		find_equal(char *str);
 void	env_to_list(t_info *info, char **env);
 void	init_struct(t_info *info, char **env);
 int		main(int argc, char **argv, char **env);
