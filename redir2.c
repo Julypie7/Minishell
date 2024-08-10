af2912f869fd066a5a603d7b12b4bbb775b6a28c
Marta López Caballer <martalop@car12s1.42barcelona.com>
2024-08-10 15:30:53 +0200

    redir2 now works with 2 redirs in and 2 redirs out

--- redir2.c
+++ redir2.c
@@ -6,7 +6,7 @@
 /*   By: martalop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 15:15:37 by martalop          #+#    #+#             */
-/*   Updated: 2024/08/08 22:40:01 by martalop         ###   ########.fr       */
+/*   Updated: 2024/08/10 15:28:54 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@
 // -> fork necesario prq hacemos execve para ejecutar el comando
 // -> redireccion (dup2) en el padre
 
-// ------     ./a.out      "<"       infile     "<"      outfile       cat       ">"      out     ------
-// ------     argv[0]   argv[1]     argv[2]   argv[3]    argv[4]     argv[5]   argv[6]  argv[7]   ------
+// -----   ./a.out      "<"       infile     "<"      outfile       cat       ">"      out      ">"      out2    ------
+// -----   argv[0]   argv[1]     argv[2]   argv[3]    argv[4]     argv[5]   argv[6]  argv[7]  argv[8]   argv[9]  ------
 
 int	open_redir(t_redir *redirs)
 {
@@ -33,7 +33,7 @@ int	open_redir(t_redir *redirs)
 	{
 		if (redirs && (redirs->token == INPUT || redirs->token == HEREDOC))
 		{
-			write(2, "entra en input\n", 15);
+		//	write(2, "entra en input\n", 15);
 			redirs->fd = open(redirs->file_name, O_RDONLY);
 			if (redirs->fd == -1)
 			{
@@ -45,7 +45,7 @@ int	open_redir(t_redir *redirs)
 		}
 		else if (redirs->token == OUTPUT)
 		{
-			write(2, "entra en outout\n", 16);
+		//	write(2, "entra en output\n", 16);
 			redirs->fd = open(redirs->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (redirs->fd == -1)
 			{
@@ -78,7 +78,7 @@ int	open_redir(t_redir *redirs)
 
 int	redirect(t_redir *redirs)
 {
-	while (redirs->next)
+/*	while (redirs->next)
 		redirs = redirs->next;
 	if (redirs->token == INPUT || redirs->token == HEREDOC)
 	{
@@ -97,51 +97,29 @@ int	redirect(t_redir *redirs)
 		}
 	}
 	close(redirs->fd);
+*/	
 	
-	
-	/*while (redirs)
+	while (redirs)
 	{
-		if (!(redirs->next))
+		if (redirs->token == INPUT || redirs->token == HEREDOC)
 		{
-			redirs
-			if (redirs && (redirs->token == INPUT || redirs->token == HEREDOC))
+			if (dup2(redirs->fd, 0) == -1)
 			{
-				if (dup2(redirs->fd, 0) == -1)
-				{
-					write(2, "dup2 failed\n", 12);
-					return (1);
-				}
+				write(2, "dup2 failed\n", 12);
+				return (1);
 			}
-			else if (redirs && redirs->token == OUTPUT)
-			{
-				if (dup2(redirs->fd, 1) == -1)
-				{
-					write(2, "dup2 failed\n", 12);
-					return (1);
-				}
-			}
-			close(redirs->fd);
 		}
+		else if (redirs->token == OUTPUT)
+		{
+			if (dup2(redirs->fd, 1) == -1)
+			{
+				write(2, "dup2 failed\n", 12);
+				return (1);
+			}
+		}
+		close(redirs->fd); // no se si es necesario
 		redirs = redirs->next;
-	}*/
-
-/*	redirs = cmd->out_redirs;
-	if (redirs && redirs->token == OUTPUT)
-	{
-		while (redirs)
-		{
-			if (!(redirs->next))
-			{
-				if (dup2(redirs->fd, 1) == -1)
-				{
-					write(2, "dup2 failed\n", 12);
-					return (1);
-				}
-				close(redirs->fd);
-			}
-			redirs = redirs->next;
-		}
-	}*/
+	}
 	return (0);
 }
 
@@ -151,6 +129,8 @@ t_cmd	*set_cmd(char **argv, char **env, t_info *info)
 	char	**arr_cmd;
 	char	*path;
 	t_redir	*tmp;
+	t_redir	*tmp2;
+	t_redir	*tmp3;
 	
 	cmd = malloc(sizeof(t_cmd) * 1);
 	if (!cmd)
@@ -188,15 +168,25 @@ t_cmd	*set_cmd(char **argv, char **env, t_info *info)
 
 	cmd->redirs->next = tmp;
 	
-	tmp = malloc(sizeof(t_redir) * 1);
-	if (!tmp)
+	tmp2 = malloc(sizeof(t_redir) * 1);
+	if (!tmp2)
 		return (NULL);
-	tmp->token = OUTPUT;
-	tmp->file_name = argv[7];
-	tmp->fd = -1;
-	tmp->next = NULL;
+	tmp2->token = OUTPUT;
+	tmp2->file_name = argv[7];
+	tmp2->fd = -1;
+	tmp2->next = NULL;
 
-	cmd->redirs->next = tmp;
+	tmp->next = tmp2;
+
+	tmp3 = malloc(sizeof(t_redir) * 1);
+	if (!tmp3)
+		return (NULL);
+	tmp3->token = OUTPUT;
+	tmp3->file_name = argv[9];
+	tmp3->fd = -1;
+	tmp3->next = NULL;
+
+	tmp2->next = tmp3;
 
 
 /*	//OUT REDIRS LIST 
@@ -223,6 +213,41 @@ t_cmd	*set_cmd(char **argv, char **env, t_info *info)
 	return (cmd);
 }
 
+void	remove_fds(t_redir *redirs)
+{
+/*	int	i;
+
+	i = 0;
+	while (i < 2 - 1) // num de redirecciones menos 1 para que no pille la última
+	{
+		close(fd[i]);
+		i++;
+	}*/
+	int		i;
+	int		j;
+	t_redir	*tmp;
+
+	i = 0;
+	j = 0;
+	tmp = redirs;
+	while (tmp)
+	{
+		if (tmp && (tmp->token == INPUT || tmp->token == HEREDOC) && i < 2 - 1)
+		{
+			write(2, "close de input fd\n", 18);
+			close(tmp->fd);
+			i++;
+		}
+		else if (tmp->token == OUTPUT && j < 1 - 1)
+		{
+			write(2, "close de output fd\n", 19);
+			close(tmp->fd);
+			j++;
+		}
+		tmp = tmp->next;
+	}
+}
+
 int	execute(t_cmd *cmd, t_info *info)
 {
 	cmd->pid = fork();
@@ -230,9 +255,9 @@ int	execute(t_cmd *cmd, t_info *info)
 		return (1);
 	if (cmd->pid == 0)
 	{
-		open_redir(cmd->redirs);
-	//	remove_fds();
-	
+		if (open_redir(cmd->redirs) == 1)
+			return (1);
+//		remove_fds(cmd->redirs);
 		redirect(cmd->redirs);
 
 		if (execve(cmd->path, cmd->arr_cmd, cmd->env) == -1)
@@ -249,15 +274,15 @@ int	execute(t_cmd *cmd, t_info *info)
 	// -> parece ser que sustituye el primer argumento del array con el path que le mandemos
 	// -> por tanto si el path es correcto, da igual cual sea el argumento, que lo ejecutará bien
 
-void	remove_fds(int	*fd)
+void	print_redirs_lst(t_redir *redirs)
 {
-	int	i;
+	t_redir	*tmp;
 
-	i = 0;
-	while (i < 2 - 1) // num de redirecciones menos 1 para que no pille la última
+	tmp = redirs;
+	while (tmp)
 	{
-		close(fd[i]);
-		i++;
+		printf("redir[%p]\ntoken: %d\nfile_name: %s\nfd: %d\n\n", tmp, tmp->token, tmp->file_name, tmp->fd);
+		tmp = tmp->next;
 	}
 }
 
@@ -270,7 +295,9 @@ int	main(int argc, char **argv, char **env)
 
 	info.ex_stat = 0;
 	cmd = set_cmd(argv, env, &info);
-	execute(cmd, &info); // FORK & EXECVE
+//	print_redirs_lst(cmd->redirs);
+	if (execute(cmd, &info) == 1) // FORK & EXECVE
+		return (1);
 	waitpid(cmd->pid, &(info.ex_stat), 0);
 	return (WEXITSTATUS(info.ex_stat));
 }
