081e771d995afe9de874d3633eee99991436c7d9
Marta López Caballer <martalop@car8s5.42barcelona.com>
2024-08-21 17:16:58 +0200

    intento de recuperacion de lex-test bueno

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/18 13:04:12 by ineimatu          #+#    #+#              #
-#    Updated: 2024/07/22 21:53:11 by martalop         ###   ########.fr        #
+#    Updated: 2024/08/21 14:15:28 by ineimatu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = minishell
 
-SRC = main.c env_utils.c env_utils2.c
+SRC = main.c env_utils.c env_utils2.c lexer_quots.c lexer.c token.c lexer_list.c
 
 OBJ = $(SRC:.c=.o)
 
@@ -20,7 +20,7 @@ CC = cc
 
 RFLAG = -lreadline
 
-CFLAGS = -Wall -Werror -Wextra -fsanitize=address
+CFLAGS = -Wall -Werror -Wextra #-fsanitize=address
 
 LIBFT_A = libft/libft.a
 
@@ -32,7 +32,7 @@ makelibft:
 $(NAME): $(LIBFT_A) $(OBJ)
 	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) -L./libft -lft $(RFLAG)
 
-%.o:%.c Makefile minishell.h
+%.o:%.c Makefile minishell.h lexer.h struct.h
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
---
+++ struct.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 13:07:01 by ineimatu          #+#    #+#             */
+/*   Updated: 2024/08/21 14:51:38 by ineimatu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+
+#include "minishell.h"
+
+typedef struct s_envp
+{
+	char 	*key;
+	char	*value;
+	struct s_envp 	*next;
+} t_envp;
+
+typedef	enum s_type
+{
+	PIPE = 1,
+	OUTPUT,
+	APPEND,
+	INPUT,
+	HEREDOC,
+} t_type;
+
+
+typedef struct s_lex_lst
+{
+	char	*word;
+	t_type	type;
+	char	*t_content;
+	//int	elem;
+	struct s_lex_lst	*next;
+}	t_lex_lst;
+
+typedef	struct s_info
+{
+	char	*rl;
+	t_lex_lst	*tokens;
+	int	ex_stat;
+	t_envp	*envp;
+} t_info;
+
+#endif
