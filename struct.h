c0e487715aff7ecb1633a8a7ff158eba147524e0
Marta López Caballer <martalop@car8s5.42barcelona.com>
2024-08-21 17:46:26 +0200

    added exec structures

--- struct.h
+++ struct.h
@@ -6,7 +6,7 @@
 /*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 13:07:01 by ineimatu          #+#    #+#             */
-/*   Updated: 2024/08/21 14:51:38 by ineimatu         ###   ########.fr       */
+/*   Updated: 2024/08/21 17:44:33 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 # define STRUCT_H
 
 #include "minishell.h"
-
-typedef struct s_envp
-{
-	char 	*key;
-	char	*value;
-	struct s_envp 	*next;
-} t_envp;
 
 typedef	enum s_type
 {
@@ -31,6 +24,44 @@ typedef	enum s_type
 	HEREDOC,
 } t_type;
 
+
+typedef struct s_redir
+{
+	t_type	token;
+	char	*file_name;
+	int		fd; // luego la quitoo
+	struct s_redir		*next;
+} 	t_redir;
+
+typedef struct s_cmd
+{
+	char	**arr_cmd;
+ 	char	*path;
+ 	char	**env;
+	int		pid;
+//	int		fd_in; // para las redirs de input
+//	int		fd_out; // para las redirs de output
+//(se van actualizando prq los voy sobreescribiendo)
+	t_redir	*redirs;
+	int		indx; // cuál comando es
+	struct s_cmd	*next;
+}	t_cmd;
+
+typedef struct s_exec
+{
+	char	**paths;
+	int		or_fd[2];
+	int		cmd_num;
+	int		pipe_end[2];
+}	t_exec;
+
+
+typedef struct s_envp
+{
+	char 	*key;
+	char	*value;
+	struct s_envp 	*next;
+} t_envp;
 
 typedef struct s_lex_lst
 {
