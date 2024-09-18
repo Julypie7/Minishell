771df53c6b6ee0ded91d1889425878f96c904a42
Marta López Caballer <martalop@car13s5.42barcelona.com>
2024-09-18 15:51:44 +0200

    norminette ok for all .h

--- lexer.h
+++ lexer.h
@@ -6,25 +6,24 @@
 /*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 10:36:08 by ineimatu          #+#    #+#             */
-/*   Updated: 2024/08/21 13:39:23 by ineimatu         ###   ########.fr       */
+/*   Updated: 2024/09/18 15:48:29 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
-#include "minishell.h"
-#include "struct.h"
+# include "minishell.h"
+# include "struct.h"
 
-
-int	valid_line(t_info *info);
-int lexer(t_info *info);
-int	add_node(char *str, char *token, t_info *info);
-int	handle_quotes(t_info *info, int i);
-int	check_token(t_info *info, int i);
-int	token_exist(char token);
+int		valid_line(t_info *info);
+int		lexer(t_info *info);
+int		add_node(char *str, char *token, t_info *info);
+int		handle_quotes(t_info *info, int i);
+int		check_token(t_info *info, int i);
+int		token_exist(char token);
 void	exit_free(char *str, int i, t_info *info);
 void	free_lexlst(t_lex_lst *tokens);
-int	handle_word(t_info *info, int i);
-void print_lex_lst(t_lex_lst *node);
+int		handle_word(t_info *info, int i);
+void	print_lex_lst(t_lex_lst *node);
 
 #endif
--- struct.h
+++ struct.h
@@ -6,43 +6,40 @@
 /*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 13:07:01 by ineimatu          #+#    #+#             */
-/*   Updated: 2024/09/16 17:21:33 by martalop         ###   ########.fr       */
+/*   Updated: 2024/09/18 15:47:03 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
 
-#include "minishell.h"
+# include "minishell.h"
 
-typedef	enum s_type
+typedef enum s_type
 {
 	PIPE = 1,
 	OUTPUT,
 	APPEND,
 	INPUT,
 	HEREDOC,
-} t_type;
-
+}	t_type;
 
 typedef struct s_redir
 {
-	t_type	type;
-	char	*file_name;
-	int		fd; // la uso para guardar heredoc
-	struct s_redir		*next;
-} 	t_redir;
+	t_type			type;
+	char			*file_name;
+	int				fd; // la uso para guardar heredoc
+	struct s_redir	*next;
+}	t_redir;
 
 typedef struct s_cmd
 {
-	char	**arr_cmd;
- 	char	*path;
- //	char	**env;
-	int		pid;
-	int		fd_in; // para las redirs de input
-	int		fd_out; // para las redirs de output
-//(se van actualizando prq los voy sobreescribiendo)
-	t_redir	*redirs;
+	char			**arr_cmd;
+	char			*path;
+	int				pid;
+	int				fd_in; // para las redirs de input
+	int				fd_out; // para las redirs de output
+	t_redir			*redirs;
 	struct s_cmd	*next;
 }	t_cmd;
 
@@ -50,35 +47,33 @@ typedef struct s_exec
 {
 	char	**paths;
 	int		or_fd[2];
-//	int		cmd_num;
- 	char	**env;
+	char	**env;
 	int		pipe_end[2];
 }	t_exec;
 
-
 typedef struct s_envp
 {
-	char 	*key;
-	char	*value;
-	struct s_envp 	*next;
-} t_envp;
+	char			*key;
+	char			*value;
+	struct s_envp	*next;
+}	t_envp;
 
 typedef struct s_lex_lst
 {
-	char	*word;
-	t_type	type;
-	char	*t_content;
+	char				*word;
+	t_type				type;
+	char				*t_content;
 	//int	elem;
 	struct s_lex_lst	*next;
 }	t_lex_lst;
 
-typedef	struct s_info
+typedef struct s_info
 {
-	char	*rl;
+	char		*rl;
 	t_lex_lst	*tokens;
-	int	ex_stat;
-	t_envp	*envp;
-	t_envp	*copy;
-} t_info;
+	int			ex_stat;
+	t_envp		*envp;
+	t_envp		*copy;
+}	t_info;
 
 #endif
